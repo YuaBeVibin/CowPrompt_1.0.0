4b74b3a3f4be5de564cbd79d32288d91dc4a8f78
wideoscarpop <81573747+wideoscarpop@users.noreply.github.com>
2021-03-29 15:24:36 +0100

    CowPrompt 1.0.0 is out!

---
+++ app.py
@@ -0,0 +1,36 @@
+print("CowPrompt 1.0.0")
+print("Type 'help' for all commands, type 'info' for software info")
+print("R U C0w?????????????????????????????")
+cow = input("Insert answer here: ")
+if cow.upper() == "YES":
+    print("https://www.youtube.com/watch?v=iXDnFYu91vY moo")
+elif cow.upper() == "MAYBE":
+    print("Weirdo")
+elif cow.upper() == "NO":
+    print("die")
+elif cow.upper() == "COW":
+    print("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjUz979yNXvAhWKShUIHTP5DXIQ3ywwAHoECAMQAg&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU")
+elif cow.upper() == "PRINGLES":
+    print("https://youtu.be/rKox3CqMX_8")
+elif cow.upper() == "SHOOP":
+    print("Shoop")
+elif cow.upper() == "OCTOBER":
+    print("https://youtu.be/PmzwhVE5Ly4")
+elif cow.upper() == "NUGGET FART":
+    print("Harely shat on you")
+elif cow.upper() == "SHIT NUGGET FART POOP":
+    print("i believe it is called the rickroll, a cultural necrophenomiconnonon that originated in south france after a mythical creature known as the sphinx decided to headbutt the Eiffel Tower which created Jean Pierre Polanareff from JoJo's Bizzare Adventure Part 3: Stardust Crusaders, Polanareff then stabbed the Nepal flag which created jacksfilms, unfortunately he got in a car accident with Jesus which created the final product that we call Call Of Duty")
+elif cow.upper() == "COPY":
+    print("https://genius.com/Copypasta-navy-seal-copypasta-annotated")
+elif cow.upper() == "WHAT'S PRISON LIKE?":
+    print("Hell")
+elif cow == "i believe it is called the rickroll, a cultural necrophenomiconnonon that originated in south france after a mythical creature known as the sphinx decided to headbutt the Eiffel Tower which created Jean Pierre Polanareff from JoJo's Bizzare Adventure Part 3: Stardust Crusaders, Polanareff then stabbed the Nepal flag which created jacksfilms, unfortunately he got in a car accident with Jesus which created the final product that we call Call Of Duty":
+    print("https://fauux.neocities.org/")
+elif cow.upper() == "help":
+    print("YES, NO, MAYBE, COW, PRINGLES, SHOOP, OCTOBER, NUGGET FART, SHIT NUGGET FART POOP, COPY, WHAT'S PRISON LIKE?")
+elif cow.upper() == "INFO":
+    print("CowPrompt version 1.0.0")
+    print("Created by Wideoscarpop, JAMES_Gaming_LV2 and Harleyinit")
+    print("This software is open source and can be reproduced by anybody as long as you credit us.")
+else:
+    print("what?")
